bbd2a33b8297f2385a65c7d7c2c26eed60757144
AndrewMShea <andrewshea06@gmail.com>
2024-02-22 02:48:22 -0600

    Update and rename base_docker_deb.sh to base_debian_vm.sh

--- base_debian_vm.sh
+++ base_debian_vm.sh
@@ -1,3 +1,7 @@
+$USER_ID=""
+$NTP_SERVER=""
+su -c "apt-get update -y && apt-get full-upgrade -y && apt-get install sudo -y && usermod -aG $USER_ID && newgrp sudo"
+su -c "  sed -i 's/#NTP=/$NTP_SERVER/' /etc/systemd/timesyncd.conf"
 sudo apt-get update -y
 sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
@@ -9,6 +13,9 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo groupadd docker
-sudo usermod -aG docker $whoami
+sudo usermod -aG docker ${SUDO_USER:-$USER}
 sudo newgrp docker
+mkdir /home/$USER/docker-container-data && mkdir /home/$USER/docker-container-compose
+docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
+sudo echo "{"log-driver":"loki","log-opts":{"loki-url":"http://mgmt-bntnkyaaloki01.andrew-shea.net:3100/loki/api/v1/push","loki-batch-size":"400"}}" > /etc/docker/daemon.json
+
