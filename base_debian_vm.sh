f6d52382a54a7e4305546d6c4cd24269dd2d5e2d
AndrewMShea <andrewshea06@gmail.com>
2024-02-22 02:58:52 -0600

    Update base_debian_vm.sh

--- base_debian_vm.sh
+++ base_debian_vm.sh
@@ -18,4 +18,16 @@ sudo newgrp docker
 mkdir /home/$USER/docker-container-data && mkdir /home/$USER/docker-container-compose
 docker plugin install grafana/loki-docker-driver:latest --alias loki --grant-all-permissions
 sudo echo "{"log-driver":"loki","log-opts":{"loki-url":"http://mgmt-bntnkyaaloki01.andrew-shea.net:3100/loki/api/v1/push","loki-batch-size":"400"}}" > /etc/docker/daemon.json
-
+mkdir /home/$USER/docker-container-compose/portainer-agent
+sudo echo "version: '3'                                                      
+services:
+  portainer_agent:
+    image: portainer/agent:2.19.4
+    ports:
+      - "$MGMT_IP:9001:9001"
+    volumes:
+      - /var/run/docker.sock:/var/run/docker.sock:rw
+      - /var/lib/docker/volumes:/var/lib/docker/volumes:rw
+    restart: unless-stopped" > /home/$USER/docker-container-compose/portainer-agent/docker-compose.yml
+cd /home/$USER/docker-container-compose/portainer-agent
+docker compose up -d
